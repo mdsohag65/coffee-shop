d26bd124334e49b501ba21c0a0f13e9c09a77df5
mdsohag65 <sohag92ahmed@gmail.com>
2022-04-27 23:26:04 +0600

    header added

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector('#menu-btn');
+// let navbar = document.querySelector('.header .flex . navbar');
+
+menu.onClick = () => {
+    menu.classList.toggle('fa-times');
+}
